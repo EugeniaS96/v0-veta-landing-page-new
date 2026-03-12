f8e0f659862136f2816802e8ffe392006173d2d2
EugeniaS96 <156593480+EugeniaS96@users.noreply.github.com>
2026-03-12 16:40:23 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-const repo = 'v0-veta-landing-page-new'
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
@@ -17,8 +14,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
 }
 
 export default nextConfig
