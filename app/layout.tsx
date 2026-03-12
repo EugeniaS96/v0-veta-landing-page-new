78399668765d8b52ad1b167e90c2d0bb1b0aafc6
EugeniaS96 <156593480+EugeniaS96@users.noreply.github.com>
2026-03-12 23:00:19 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,19 +67,16 @@ export default function RootLayout({
             (function(m,e,t,r,i,k,a){
               m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
               m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-              }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+              k.async=1;k.src=r;a.parentNode.insertBefore(k,a)
+            })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(106855911, 'init', {
-              ssr: true,
-              webvisor: true,
-              clickmap: true,
-              ecommerce: 'dataLayer',
-              accurateTrackBounce: true,
-              trackLinks: true
+            ym(106855911, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
             });
           `}
         </Script>
